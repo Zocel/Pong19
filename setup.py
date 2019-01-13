50934bdca405dc137ce72a9e53702c121bd5abfb
Yann Michel LE COZ <yann.lecoz@ynov.com>
2019-01-13 18:50:43 +0100

    UPDATE 13/01/2019

--- setup.py
+++ setup.py
@@ -6,25 +6,17 @@
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    readme = fh.read()
 
 setup(
-    name='Pong19',
-    version='1.0',
     author="Yann Michel Le Coz",
     author_email='yann.lecoz@ynov.com',
-    description="Cette application permet de jouer au jeu d'arcade Pong.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/Zocel/Pong19',
-    packages=find_packages(),
-    license="GNU General Public License v3",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        "Natural Language :: French",
         'Natural Language :: English',
+        "Natural Language :: French",
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
@@ -33,4 +25,15 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
+    description="Cette application permet de jouer au jeu d'arcade Pong.",
+    license="GNU General Public License v3",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords='Pong19',
+    name='Pong19',
+    packages=find_packages(),
+    url='https://github.com/Zocel/Pong19',
+    version = '0.1.1',
+    zip_safe=False,
 )
